e1e2d15fe554bbc0e187d71237ae84071a600bef
SRNelsonR <oscarnelson18@gmail.com>
2018-11-15 14:28:12 -0600

    Carpinteria

--- Edusystem/resources/views/pagos_meses_universidad/index.blade.php
+++ Edusystem/resources/views/pagos_meses_universidad/index.blade.php
@@ -12,7 +12,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Pago meses Universidades</h2>
+                    <h2>Pago Meses Universidades</h2>
                    
                     <div class="clearfix"></div>
                   </div>
